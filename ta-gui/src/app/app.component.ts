6b2d67d002319ef061765b55fd348be1263d420f
pauloborba <phmb@cin.ufpe.br>
2017-09-03 18:13:56 -0300

    botao e funcionalidade do formulario para cadastro de alunos

--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NgModule } from '@angular/core';
 
+import { Aluno } from './aluno';
+import { AlunoService } from './aluno.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,6 +11,7 @@ import { NgModule } from '@angular/core';
 })
 
 export class AppComponent {
+<<<<<<< HEAD
    title = 'ta-gui';
    aluno: Aluno = {nome: "", cpf: "", email: "", loginGit: ""};
 }
@@ -17,4 +21,13 @@ export class Aluno {
   cpf: string;
   email: string;
   loginGit: string;
+=======
+   aluno: Aluno = {nome: "", cpf: "", email: ""};
+   alunoService = new AlunoService();
+
+   gravar(a: Aluno): void {
+     this.alunoService.gravar(a);
+     this.aluno = {nome: "", cpf: "", email: ""};
+  }
+>>>>>>> ab841e1... botao e funcionalidade do formulario para cadastro de alunos
 }
