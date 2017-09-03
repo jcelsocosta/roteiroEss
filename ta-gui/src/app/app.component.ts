3d4d8f7e52812b39444eea02c61495f5accb8e40
pauloborba <phmb@cin.ufpe.br>
2017-09-03 18:26:56 -0300

    alerta para CPF já cadastrado

--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -17,9 +17,6 @@ export class AppComponent {
    alunos: Aluno[] = [];
 
    gravar(a: Aluno): void {
-     this.alunoService.gravar(a);
-     this.alunos.push(a);
-     this.aluno = {nome: "", cpf: "", email: "", loginGit: ""};
      if (this.alunoService.gravar(a)) {
        this.alunos.push(a);
        this.aluno = {nome: "", cpf: "", email: "", loginGit: ""};
