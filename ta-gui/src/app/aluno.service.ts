699995f5436f354c1da158f9ea8455f107d94b9c
pauloborba <phmb@cin.ufpe.br>
2017-09-05 18:34:00 -0300

    cadastro de metas, e clonagem de objetos

--- ta-gui/src/app/aluno.service.ts
+++ ta-gui/src/app/aluno.service.ts
@@ -6,7 +6,8 @@ import { Aluno } from './aluno';
 export class AlunoService {
   alunos: Aluno[] = [];
 
-  gravar(aluno: Aluno): Aluno {
+  criar(aluno: Aluno): Aluno {
+    aluno = aluno.clone();
     var result = null;
     if(!this.alunos.find(a => a.cpf == aluno.cpf)){
     if (this.cpfNaoCadastrado(aluno.cpf)) {
@@ -16,7 +17,17 @@ export class AlunoService {
     return result;
   }
 }
+
   cpfNaoCadastrado(cpf: string): boolean {
      return !this.alunos.find(a => a.cpf == cpf);
+  }
+
+  atualizar(aluno:Aluno): void {
+    aluno = aluno.clone();
+    for (let a of this.alunos) {
+        if (a.cpf == aluno.cpf) {
+           a.metas = aluno.metas;
+        }
+    }
   }
 }
