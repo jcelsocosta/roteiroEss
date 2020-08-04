729fcba5a070be756666e8d3b2e8231a0fca92b9
jcelsocosta <jccl@cin.ufpe.br>
2020-08-04 00:01:35 -0300

    evitar cadastro de mais de um aluno com o mesmo cpf

--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -15,42 +15,16 @@ export class AppComponent {
    aluno: Aluno = {nome: "", cpf: "", email: "", loginGit: ""};
    alunoService = new AlunoService();
    alunos: Aluno[] = [];
-   gravar(a: Aluno): void {
-    this.alunoService.gravar(a);
-    this.alunos.push(a);
-    this.aluno = {nome: "", cpf: "", email: "", loginGit: ""};
- }
-}
-/**
-export class Aluno {
-  
-  nome: string;
-  cpf: string;
-  email: string;
-  loginGit: string;
-   aluno: Aluno = {nome: "", cpf: "", email: "",loginGit: ""};
-   alunoService = new AlunoService();
-   alunos: Aluno[] = [];
 
    gravar(a: Aluno): void {
-<<<<<<< HEAD
-<<<<<<< HEAD
      this.alunoService.gravar(a);
      this.alunos.push(a);
      this.aluno = {nome: "", cpf: "", email: "", loginGit: ""};
-=======
-=======
->>>>>>> 573dd47... evitar cadastro de mais de um aluno com o mesmo cpf
      if (this.alunoService.gravar(a)) {
        this.alunos.push(a);
-       this.aluno = {nome: "", cpf: "", email: ""};
+       this.aluno = {nome: "", cpf: "", email: "", loginGit: ""};
      } else {
        this.aluno.cpf = "";
      }
-<<<<<<< HEAD
->>>>>>> 573dd47... evitar cadastro de mais de um aluno com o mesmo cpf
-=======
->>>>>>> 573dd47... evitar cadastro de mais de um aluno com o mesmo cpf
-  }
-  
-}*/
+ }
+}
